36748aba939ea4e20f42503c12e2e43e99b19bc4
Eugene Dobry <eugene.dobry@massexchange.com>
2016-05-13 19:17:18 -0400

    improved/converted deleeGroups

---
+++ deleteGroups.js
@@ -0,0 +1,22 @@
+module.exports = mpId => `
+SET @mpId = ${mpId};
+
+drop temporary table if exists CollapsedToDelete;
+create temporary table if not exists CollapsedToDelete as (
+	select id from Collapsed_Catalog_Records
+	where mpId = @mpId
+);
+
+delete ccrcr
+from Collapsed_Catalog_Record_Catalog_Records ccrcr, CollapsedToDelete c2d
+where ccrcr.collCatRecId = c2d.id;
+
+delete cgccr
+from Catalog_Group_Collapsed_Catalog_Records cgccr, CollapsedToDelete c2d
+where cgccr.collCatRecId = c2d.id;
+
+delete ccr
+from Collapsed_Catalog_Records ccr, CollapsedToDelete c2d
+where ccr.id = c2d.id;
+
+delete from Catalog_Groups where mpId = @mpId;`;
