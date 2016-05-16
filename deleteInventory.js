a8d28f2703b9f408e9da7b9e862ad7ca7ac87ae7
Eugene Dobry <eugene.dobry@massexchange.com>
2016-05-16 12:48:42 -0400

    refactored delete scripts

--- deleteInventory.js
+++ deleteInventory.js
@@ -1,18 +1,28 @@
+var tempTable = require("./tempTable");
+
 module.exports = mpId  => `
 set @mpId = ${mpId};
 
 delete from Catalog_Records where mpId = @mpId;
 
-drop temporary table if exists InvToDelete;
-create temporary table if not exists InvToDelete as
-	(select id, sourceId from Inventory where mpId = @mpId);
+${tempTable("InvToDelete", `
+	select id, sourceId
+	from Inventory
+	where mpId = @mpId
+`)}
 
 delete i
-from Inventory i, InvToDelete i2d
+from
+	Inventory i,
+	InvToDelete i2d
 where i.id = i2d.id;
 
 delete ds, s
-from Dfp_Inventory_Sources ds, Inventory_Sources s, InvToDelete i2d
+from
+	Dfp_Inventory_Sources ds,
+	Inventory_Sources s,
+	InvToDelete i2d
 where
 	ds.id = s.id and
-	s.id = i2d.sourceId;`;
+	s.id = i2d.sourceId;
+`;
