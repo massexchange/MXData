0719e94b5b9fca7123677004db4c14f9eabd24db
Frank Camilleri <fcamilleri22@gmail.com>
2017-01-31 17:34:02 -0500

    WIP

--- mxsshtools/mxip/mxip.js
+++ mxsshtools/mxip/mxip.js
@@ -5,7 +5,7 @@ var EC2 = new AWS.EC2();
 var args = process.argv.slice(2);
 
 
-getRelevantEC2InstanceByName = function(instanceName){
+const getEC2InstanceByName = function(instanceName){
 	var params = {
 		"Filters":[{
 			"Name":"tag-key",
@@ -25,6 +25,13 @@ getRelevantEC2InstanceByName = function(instanceName){
 	})
 };
 
-getRelevantEC2InstanceByName(args[0]).on("success", function(response){
-	console.log(response.data.Reservations[0].Instances[0].PublicIpAddress);
-});
+const main = () => {
+	if (!args[0]) {
+		console.log("Error: No instance name given. Exiting.");
+		process.exitCode = 1;
+		return
+	}
+	getEC2InstanceByName(args[0]).on("success", function(response){
+		console.log(response.data.Reservations[0].Instances[0].PublicIpAddress);
+	});
+}
