e1e8b4454825554a987e6d34f0a0a9d9efe1e120
Frank Camilleri <fcamilleri22@gmail.com>
2017-02-06 16:24:27 -0500

    Added protection for the terminated/multi exists cases

--- mxsshtools/mxip/mxip.js
+++ mxsshtools/mxip/mxip.js
@@ -1,37 +1,51 @@
-var AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
 AWS.config.loadFromPath("/etc/awsinfo");
-var EC2 = new AWS.EC2();
+const EC2 = new AWS.EC2();
 
-var args = process.argv.slice(2);
+const arg = process.argv.slice(2)[0];
 
 
 const getEC2InstanceByName = function(instanceName){
-	var params = {
-		"Filters":[{
-			"Name":"tag-key",
-			"Values":["Name"]
-		},
-		{
-			"Name":"tag-value",
-			"Values":[instanceName]
-		}
-		]
-	};
-	return EC2.describeInstances(params, function(err,data){
-		if (err){
-			console.log(err, err.stack);
-			console.log("getRelevantEC2InstancesByEnvironment failed");
-		}
-	})
+    var params = {
+        "Filters":[
+            {
+                "Name":"tag-key",
+                "Values":["Name"]
+            },
+            {
+                "Name":"tag-value",
+                "Values":[instanceName]
+            }
+        ]
+    };
+    return EC2.describeInstances(params).promise();
 };
 
 const main = () => {
-	if (!args[0]) {
-		console.log("Error: No instance name given. Exiting.");
-		process.exitCode = 1;
-		return
-	}
-	getEC2InstanceByName(args[0]).on("success", function(response){
-		console.log(response.data.Reservations[0].Instances[0].PublicIpAddress);
-	});
-}
+    if (!arg) {
+        console.log("Error: No instance name given. Exiting.");
+        process.exitCode = 1;
+        return;
+    }
+    getEC2InstanceByName(arg)
+    .then(response => {
+        //first filter out only whats running.
+        let reservations = response.Reservations;
+        let goodReservations = reservations.filter(reservation => {
+            return reservation.Instances[0].State.Name == "running";
+        });
+        if (goodReservations.length == 1){ //we have exactly one good one.
+            console.log(goodReservations[0].Instances[0].PublicIpAddress);
+        } else if (goodReservations.length > 1){ //we have more than one good one.
+            console.log("Multiple instances for that name found.");
+            console.log("Please alert Ops regarding this issue, as instance names should be unique.");
+            process.exitCode = 1;
+        } else { //none found
+            console.log("No instances for that name found.");
+            console.log("The instance is either off or doesn't exist.");
+            process.exitCode = 1;
+        }
+    });
+};
+
+main();
