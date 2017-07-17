38e290e84fc972e12d04f169c6d67d81aa3d8dc2
edobry <eugene@dobry.me>
2017-07-17 17:03:51 -0400

    run each script in a transaction

--- render.js
+++ render.js
@@ -1,6 +1,21 @@
+const
+    { sql } = require("./helpers/util");
+
 module.exports = (scriptName, params) => {
     if(!scriptName)
         throw new Error("Script name must be specified");
 
-    return require(`./scripts/${scriptName.split("/")[1]}`).apply(null, params);
+    const name = scriptName.split("/")[1];
+
+    const script = require(`./scripts/${name}`);
+
+    const result = sql`
+        START TRANSACTION;
+
+        ${script(...params)}
+
+        COMMIT;
+    `;
+
+    return result;
 };
